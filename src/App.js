646bd7393beec3601a361a9a4713060cf66fc655
Mai Vang <vmaineng@gmail.com>
2024-01-18 17:39:54 -0800

    added in images

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { NavBar } from './components/NavBar';
-import { Banner } from './components/Banner';
-import {Projects} from './components/Projects'
-import { Footer } from './components/Footer';
-
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { NavBar } from "./components/NavBar";
+import { Banner } from "./components/Banner";
+import { Projects } from "./components/Projects";
+import { Footer } from "./components/Footer";
 
 class App extends Component {
   render() {
@@ -14,11 +13,7 @@ class App extends Component {
         <NavBar />
         <Banner />
         <Projects />
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-    <Footer />
+        <Footer />
       </div>
     );
   }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import githubIcon from '../assets/nav/github.png';
+import linkedinIcon from '../assets/nav/linkedIn.png';
+
 
 export const NavBar = () => {
   return (
